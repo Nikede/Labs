20be32b30957c92c52857629caa0d9840db5bad0
Andrew Kondratyev <andrewkondratyevs@gmail.com>
2018-09-09 13:24:35 +0300

    Complete Task1-2

--- task1-2/registration.js
+++ task1-2/registration.js
@@ -6,12 +6,12 @@ function checkEmail(str){
   }return true;
 }
 
-function checkPassword(pass, confrim) {
+function checkPassword(pass, confirm) {
   if (pass.length < 6) {
     alert('Длина пароля должна быть не менее 6-ти символов.');
     return false;
   }
-  if (pass = confrim) {
+  if (pass !== confirm) {
     alert('Введенные пароли не совпадают.');
     return false;
   }
@@ -19,7 +19,7 @@ function checkPassword(pass, confrim) {
 }
 
 function checkAgreementRules() {
-  var result = document.getElementsByClassName("registration__check-confirmation")[0].checked;
+  var result = document.getElementsByClassName("registration_check_confirmation")[0].checked;
   if (!result) {
     alert("Примите условия соглашения!");
     return false;
@@ -27,17 +27,16 @@ function checkAgreementRules() {
   return true;
 }
 
-function registration();
+function registration()
 {
-  var email = document.getElementsByClassName("registration__login")[0].value;
-  var pass = document.getElementsByClassName("password-block__password")[0].value;
-  varpassConfirm = document.getElementsByClassName("password-block__password-repeat")[0].value;
-  if (checkEmail(email) && checkPassword(pass, passConfirm)
-      && checkAgreementRules()) {
+  var email = document.getElementsByClassName("registration_login")[0].value;
+  var pass = document.getElementsByClassName("password_block_password")[0].value;
+  var passConfirm = document.getElementsByClassName("password_block_password_repeat")[0].value;
+  if (checkEmail(email) && checkPassword(pass, passConfirm) && checkAgreementRules()) {
     alert("Регистрация прошла успешно!");
   }
 }
 
 document.getElementsByClassName("registration")[0].onsubmit = function() {
-  Registration();
+  registration();
 };
