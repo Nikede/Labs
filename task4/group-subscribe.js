4bbb49cf988da25b8344880c05456afcd945572d
Andrew Kondratyev <andrewkondratyevs@gmail.com>
2018-09-09 19:28:22 +0300

    Complete Task4

--- task4/group-subscribe.js
+++ task4/group-subscribe.js
@@ -10,8 +10,14 @@ const groupList = [
  * @param group
  */
 function subscribeGroup(group) {
-  // todo
-  myGroups.push(group);
+  let exists = false;
+  groupList.forEach(function(item){
+    if (item.groupName === group) {
+      myGroups.push(group);
+      exists = true;
+    }
+  });
+  if (!exists) alert("Такой группы не существует");
 }
 
 /**
@@ -19,5 +25,9 @@ function subscribeGroup(group) {
  * @param group
  */
 function unsubscribeGroup(group) {
-  // todo
+  if ((myGroups.length > 0) && (myGroups.indexOf(group) >= 0)) {
+    myGroups.splice(myGroups.indexOf(group), 1);
+  } else {
+    alert("Вы не подписаны на данную группу");
+  }
 }
