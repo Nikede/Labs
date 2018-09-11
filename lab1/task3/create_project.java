17af5e277735c421066ba741115e64f881931cd6
Andrew Kondratyev <andrewkondratyevs@gmail.com>
2018-09-11 08:46:29 +0300

    Redirecting CSS and JS files

--- lab1/task3/create_project.java
+++ lab1/task3/create_project.java
@@ -4,22 +4,25 @@ public class create_project {
 
   public static void main(String[] args) throws Exception {
 
+    File css_folder = new File("css");
+    File js_folder = new File("js");
+
     File html = new File(System.getProperty("user.dir"), "index.html");
-    File css = new File(System.getProperty("user.dir"), "css.css");
-    File js = new File(System.getProperty("user.dir"), "js.js");
+    File css = new File(css_folder.toString(), "style.css");
+    File js = new File(js_folder.toString(), "app.js");
 		
     String html_string = "<!DOCTYPE html>\r\n" + 
       "<html lang=\"en\">\r\n" + 
       "<head>\r\n" + 
       "  <meta charset=\"UTF-8\">\r\n" + 
       "  <title>Title</title>\r\n" + 
-      "  <link rel=\"stylesheet\" href=\"css.css\"/>\r\n" + 
+      "  <link rel=\"stylesheet\" href=\"css/style.css\"/>\r\n" + 
       "</head>\r\n" + 
       "<body>\r\n" + 
       "  <div class=\"content\">\r\n" + 
       "    <h1>Hello! I'm ready for edit! :)</h1>\r\n" + 
       "  </div>\r\n" + 
-      "  <script src=\"js.js\"></script>\r\n" + 
+      "  <script src=\"js/app.js\"></script>\r\n" + 
       "</body>\r\n" + 
       "</html>";
 	  
@@ -53,14 +56,20 @@ public class create_project {
       html_fw.flush();
     } else System.out.println("HTML file already exists");
 	
-    if (!css.exists()) {
+    if (!css_folder.exists()) {
+      css_folder.mkdir();
+    } 
+    if (!css.exists()){
       done &= css.createNewFile();
       FileWriter css_fw = new FileWriter(css, false);
       css_fw.write(css_string);
       css_fw.flush();
     } else System.out.println("CSS file already exists");
 	
-    if (!js.exists()) {
+    if (!js_folder.exists()) {
+      js_folder.mkdir();
+    }
+    if (!js.exists()){
       done &= js.createNewFile();
       FileWriter js_fw = new FileWriter(js, false);
       js_fw.write(js_string);
